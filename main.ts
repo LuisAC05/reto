c95f6b923dbd37d4bb71ef474e9b5b970566bd75
LuisAC05 <112582865+LuisAC05@users.noreply.github.com>
2022-09-02 12:46:17 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    N1 = 0
+    basic.showNumber(N1 + 1)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("HOLA")
+})
+let N1 = 0
+N1 = randint(0, 10)
+let N2 = randint(0, 10)
